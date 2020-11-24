bd23a66beb39245599dccfdc04bcbd00ae421639
Jan Sarenik <jajomojo@gmail.com>
2020-11-24 08:26:10 +0100

    Add sedfix.sh

---
+++ sedfix.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+find . -name '*.html' \
+  | while read i
+do
+  sed -i \
+    -e 's|https?://DVzine.org.*"|index.html"|g' \
+    -e 's|https?://dvzine.org.*"|index.html"|g' \
+    -e 's|https?://www.DVzine.org.*"|index.html"|g' \
+    -e 's|https?://www.dvzine.org.*"|index.html"|g' \
+    -e 's|http://|https://|g' \
+    $i
+done
