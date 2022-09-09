b45e50401aa0d20ee61135b13d0f3be0e05d3042
Jan Sarenik <jajomojo@gmail.com>
2022-09-09 15:40:51 +0200

    sedfix.sh: Use -E for regex

--- sedfix.sh
+++ sedfix.sh
@@ -3,11 +3,12 @@
 find public -name '*.html' \
   | while read i
 do
-  sed -i \
-    -e 's|https?://DVzine.org.*"|index.html"|g' \
-    -e 's|https?://dvzine.org.*"|index.html"|g' \
-    -e 's|https?://www.DVzine.org.*"|index.html"|g' \
-    -e 's|https?://www.dvzine.org.*"|index.html"|g' \
+  sed -Ei \
+    -e 's|https?://DVzine.org.*"|/"|g' \
+    -e 's|https?://dvzine.org.*"|/"|g' \
+    -e 's|https?://www.DVzine.org.*"|/"|g' \
+    -e 's|https?://www.dvzine.org.*"|/"|g' \
+    -e 's|/index.html"|/"|g' \
     -e 's|http://|https://|g' \
     $i
 done
